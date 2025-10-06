ca3c3b8c70f4c7034059888f6d9d3ad830568c76
vargalott <156085832+vargalott@users.noreply.github.com>
2025-10-06 17:01:38 +0300

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,52 @@
+# Configure prompt
+function prompt {
+    $currentDateTime = [DateTime]::Now.ToString("yyyy-MM-dd HH:mm:ss")
+    $user = [Environment]::UserName
+    $hostName = [Environment]::MachineName
+    $currentPath = Get-Location
+
+    # Colors
+    Write-Host -NoNewline -ForegroundColor Magenta "$currentDateTime "
+    Write-Host -NoNewline -ForegroundColor DarkRed "$user@$hostName "
+    Write-Host -NoNewline -ForegroundColor Blue "$currentPath "
+    Write-Host -NoNewline -ForegroundColor White "->"
+    return " "
+}
+
+# $PSROptions = @{
+#     ContinuationPrompt = " "
+#     Colors = @{
+#         Operator = $PSStyle.Foreground.Magenta
+#         Parameter = $PSStyle.Foreground.Magenta
+#         InLinePrediction = $PSStyle.Foreground.DarkGrey + $PSStyle.Background.BrightBlack
+#     }
+# }
+# Set-PSReadLineOption @PSROptions
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle InlineView
+
+Set-Alias .. Set-Location
+function ... { Set-Location ../.. }
+function .... { Set-Location ../../.. }
+function ll {
+    Get-ChildItem -Force | Sort-Object { -not $_.PSIsContainer }, Name | Format-Table Mode, LastWriteTime, Length, Name
+}
+function ducks {
+    Get-ChildItem | ForEach-Object {
+        $size = (Get-ChildItem $_.FullName -Recurse -File -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum).Sum
+        if (-not $size) { $size = 0 }
+
+        $hrSize = switch ($size) {
+            {$_ -ge 1GB} { "{0:N1} GB" -f ($size/1GB); break }
+            {$_ -ge 1MB} { "{0:N1} MB" -f ($size/1MB); break }
+            {$_ -ge 1KB} { "{0:N1} KB" -f ($size/1KB); break }
+            default { "0 B" }
+        }
+
+        return [PSCustomObject]@{
+            Name = $_.Name
+            Size = $hrSize
+            RawSize = $size
+        }
+    } | Sort-Object RawSize -Descending | Format-Table Name, Size
+}
